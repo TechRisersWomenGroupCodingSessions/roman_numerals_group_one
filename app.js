af7d505bf332a0bd6b3b6e322be74ec631e02bc7
clarice_r <claricerdallorto@gmail.com>
2024-07-24 18:42:52 +0100

    Test 5 is V

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 function convert_to_roman_numerals(number) {
-    return number;
+    switch(number) {
+      case 1:
+        roman_numeral = "I"
+    }
+    return roman_numeral;
   }
   module.exports = convert_to_roman_numerals;
--- app.test.js
+++ app.test.js
@@ -3,3 +3,7 @@ const convert_to_roman_numerals = require('./app');
 test('converts 1 to I', () => {
   expect(convert_to_roman_numerals(1)).toBe("I");
 });
+
+test('converts 5 to V', () => {
+  expect(convert_to_roman_numerals(5)).toBe("V");
+});
